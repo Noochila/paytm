d65c0968f82549c85c56cd4927d3f79e4347ea5b
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-11-03 17:45:42 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -39,7 +39,7 @@ export default function Signup() {
             <Button onclick={async () => {
 
                 try {
-                    const response = await axios.post("https://paytm-9qy0.onrender.com/api/v1/user/signup", {
+                    const response = await axios.post(PROCESS.env.BACKEND_URL+"/api/v1/user/signup", {
                         username,
                         email,
                         password,
